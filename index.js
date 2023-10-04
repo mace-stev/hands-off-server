05de2bef4c160df5dfccce49870477f7e2319102
Mason Stevenson <113743335+mace-stev@users.noreply.github.com>
2023-10-04 00:17:35 -0500

    Update index.js

--- index.js
+++ index.js
@@ -11,8 +11,8 @@ const profileRoutes=require('./routes/profileRoute')
 app.use(fileupload());
 app.use(cors());
 app.use(express.json());
-app.use('/', videoRoutes)
-app.use('/', profileRoutes)
+app.use('//', videoRoutes)
+app.use('//', profileRoutes)
 app.listen(process.env.PORT, ()=>{
 console.log("Here")
 })
