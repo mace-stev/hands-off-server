872d18813e2e58c584a4b5b4818c842f99a44f6b
Mason Stevenson <williamstevenson107@gmail.com>
2023-11-05 19:27:12 -0600

    It has taken me so long, but I finally uploaded a complete youtube video along with it's details such as title and description; I guess this taught me to be good to my fellow developer and have good documentation for my projects. I have been trying to upload for so long based off of an example in YouTube's docs and I just found out Youtube no longer allows this method even though there is no indication in the docs.

--- controllers/videosController.js
+++ controllers/videosController.js
@@ -1,71 +1,84 @@
 const fs = require('fs');
 const fileupload = require("express-fileupload");
 const path = require('node:path')
-const FormData = require('form-data');
 const axios = require('axios')
 const cliProgress = require('cli-progress');
+
 exports.recording = (req, res) => {
   const progressBar = new cliProgress.SingleBar({}, cliProgress.Presets.shades_classic);
-  let fileData = []
-  const stats = []
-  let uploadedBytes = 0;
-  
-  const postData = req.body
-  console.log(req.body)
+  let fileData = [];
+  const stats = [];
+  const postData = req.body;
+  console.log(req.body);
   new Promise((resolve, reject) => {
     fs.readdir(`${req.body.recordingFolder}`, (err, files) => {
       if (err) {
-        reject("videosController.js line 21: " + err)
+        reject("videosController.js line 21: " + err);
       }
 
       files.forEach((element, index) => {
         const { size, mtimeMs } = fs.statSync(`${req.body.recordingFolder}\\${element}`);
         stats.push({ size, mtimeMs, element });
-      })
+      });
 
       fileData = stats.filter((item) => {
         if (item.mtimeMs === Math.max.apply(null, stats.map(function (o) { return o.mtimeMs; }))) {
           return item;
         }
       });
-      resolve(
-        fileData[0].element
-      )
-    })
-  }).then((response) => {
-    console.log(response)
-    const completePath = (`${req.body.recordingFolder}\\${response}`)
-    const formData= new FormData()
-    console.log(completePath);
-    axios.defaults.headers.common = null;
-    const data = fs.createReadStream(completePath)
-    progressBar.start(fileData[0].size, 0);
-    data.on('data', chunk => {
-      uploadedBytes += chunk.length;
-      progressBar.update(uploadedBytes);
-    });
-    data.on('end', () => {
-      progressBar.stop();
-    });
-    console.log(req.body.snippetData)
-    
-    
-    formData?.append('snippet', JSON.stringify(req.body.snippetData))
-    formData?.append('video', data)
 
-    
-    return axios.post('https://www.googleapis.com/upload/youtube/v3/videos?uploadType=multipart&part=snippet&mine=true', formData, { headers: { 'Content-Type': 'multipart/form-data', 'Authorization': `${req.headers.authorization}` } })
-      .then((result) => {
-        console.log(result);
-        res.status(201).json(`Successfully posted the video: ${result}`);
-      })
-      .catch((err) => {
-        console.log(err + " line 51");
-        res.status(500).json(`(videosController.js line 64): Error uploading video: ${err}`);
+      resolve(fileData[0].element);
+    });
+  }).then((response) => {
+    console.log(response);
+    const completePath = (`${req.body.recordingFolder}\\${response}`);
+    const video = fs.readFileSync(completePath);
+    axios.defaults.headers.common = null;
+    const url = 'https://www.googleapis.com/upload/youtube/v3/videos';
+
+    axios.post(`${url}?uploadType=resumable&part=snippet`, {snippet: req.body.snippetData}, {
+      headers: {
+        'Content-Type': 'application/json; charset=UTF-8',
+        'Authorization': `${req.headers.authorization}`,
+        'X-Upload-Content-Length': `${video.length}`,
+        'X-Upload-Content-Type': 'application/octet-stream'
+      },
+    }).then((response) => {
+      const uploadId = response.headers['x-guploader-uploadid'];
+      console.log(response)
+      // Send the entire video data as a single request
+      axios.put(`${url}?uploadType=resumable&upload_id=${uploadId}`, video, {
+        headers: {
+          'Authorization': `${req.headers.authorization}`,
+          'Content-Type': 'application/octet-stream',
+          'Content-Length': `${video.length}`
+        },
+        onUploadProgress: (progressEvent) => {
+          const progress = (progressEvent.loaded / progressEvent.total) * 100;
+          console.log(`Upload progress: ${progress}%`);
+        },
+      }).then((response) => {
+        // Complete the upload
+        return axios.put(`${url}?uploadType=resumable&part=status&status=processing&upload_id=${uploadId}`, {
+          headers: {
+            'Authorization': `${req.headers.authorization}`,
+            'Content-Type': 'application/json',
+            'Content-Length': `${video.length}`
+          },
+        }).then((response) => {
+          console.log('Video uploaded successfully:', response);
+        }).catch((error) => {
+          console.error('Error uploading video:', error);
+        });
+      }).catch((error) => {
+        console.error('Error uploading video data:', error);
       });
+    }).catch((error) => {
+      console.error('Error uploading snippet data:', error);
+    });
   });
 };
-  
-  exports.testing = (req, res) => {
+
+exports.testing = (req, res) => {
   res.json("I\'m listening")
 }
